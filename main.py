379bb8e6939ffd20ffbf1f84fdb9a695b4e4e163
LeEHaNa1 <127467192+LeEHaNa1@users.noreply.github.com>
2023-03-24 13:59:29 +0900

    Add another code
    새롭게 소개하는 코드 하나 추가했습니다!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("X")
 print("hi world!")
 print('godbye world!')
-
+print("Hello, This is hellosehyun's repository!")
 
 print('change')
 print('박세현 202201396 https://github.com/hellosehyun/test-repo')
